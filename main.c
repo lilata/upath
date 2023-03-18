87e8eba558a9fffaa52ebaf04d06d4fbacc27a2b
Wang Zhao <wz@wangzhao.rocks>
2023-03-18 19:08:48 +0800

    define path siez

--- main.c
+++ main.c
@@ -1,22 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#define PATH_SIZE 375
 int main() {
     char* path = getenv("PATH");
     char* entry;
-    const char* entries[500];
-    for (int i = 0; i < 500; ++i) {
+    const char* entries[PATH_SIZE];
+    for (int i = 0; i < PATH_SIZE; ++i) {
         entries[i] = "\0";
     }
     int idx = 0;
     while ((entry = strsep(&path, ":"))) {
         int should_add = 1;
-        for (int i = 0; i < 500; ++i) {
-            if (entries[i] == entry) {
+        for (int i = 0; i < PATH_SIZE; ++i) {
+            if (strcmp(entries[i], entry) == 0) {
                 should_add = 0;
                 break;
             }
-            if (!strcmp(entries[i], "\0")) {
+            if (strcmp(entries[i], "\0") == 0) {
                 break;
             }
         }
@@ -25,7 +26,7 @@ int main() {
             ++idx;
         }
     }
-    for (int i = 0; i < 500; ++i) {
+    for (int i = 0; i < PATH_SIZE; ++i) {
         if (strcmp(entries[i], "\0") == 0) break;
         printf("%s", entries[i]);
         if (strcmp(entries[i + 1], "\0") != 0) {
